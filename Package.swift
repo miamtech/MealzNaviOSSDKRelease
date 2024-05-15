43ae02cbe4a9ce0d0c848c14115059df92e83abe
walerowicz-miam <damien.walerowicz@mealz.ai>
2024-05-15 11:01:07 +0200

    update release

--- Package.swift
+++ Package.swift
@@ -3,22 +3,31 @@
 
 import PackageDescription
 
+let configurationMode = "prod"
+
 let package = Package(
-    name: "MealzNaviOSSDKRelease",
+    name: "MealzNaviOSSDK",
     defaultLocalization: "fr",
     platforms: [
         .iOS(.v12),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MealzNaviOSSDKRelease",
-            targets: ["MealzNaviOSSDKRelease"]),
+            name: "MealzNaviOSSDK",
+            targets: ["MealzNaviOSSDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/miamtech/MealzIOSFrameworkRelease", from: "1.0.0")
     ],
     targets: [
         .binaryTarget(
-            name: "MealzNaviOSSDKRelease",
-            path: "./MealzNaviOSSDK.xcframework"
+            name: "MealzNaviOSSDK",
+            url: "https://github.com/miamtech/MealzNaviOSSDKRelease/raw/release/1.0.1/MealzNaviOSSDK.zip",
+            checksum: "eb0cbe390ff3b1f10144262ef07df93e6e6502ee3d3b02728c7e42321e7c9f45"
         )
+        
     ]
 )
+/*
+ 
+ */
